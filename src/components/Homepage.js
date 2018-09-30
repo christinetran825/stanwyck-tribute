adf4228d23809977cca544425dff6c1a4a34a4eb
github username <github email address>
2018-09-30 12:27:51 -0700

    imported Beginnings component

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Beginnings from './Beginnings'
 
 class Homepage extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <main>
+        <Beginnings/>
+      </main>
     );
   }
 }
