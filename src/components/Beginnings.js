715fd1727e50d1a7accf171d5de52622ec5194c6
github username <github email address>
2018-09-30 12:27:33 -0700

    added content, photo reference, and quote on early life

---
+++ src/components/Beginnings.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Beginnings extends Component {
+  render() {
+    return (
+      <div id="early-beginnings" className="col-2">
+        <div className="story">
+          <p><span class="dropcap">B</span>orn as Ruby Catherine Stevens to a poor Scottish-Irish working class family in Brooklyn in July 16, 1907. When Ruby was just four, her mother was killed after a drunk knocked her off a moving streetcar. The loss of her mother devastated her grieving father. Unable to cope with such lost he vanished without a word. The children never saw him again.</p>
+
+          <p>Barbara’s older sister, Mildred, served as the mother figure to her siblings. When Mildred found work as a showgirl in a traveling theater troupe, Ruby and her siblings were place in a series of foster homes. Though during the summers, Ruby was allowed to visit her sister on tour. From these visits, Ruby was captivated by the stage, bright lights, and energy. She practiced her sister’s routines backstage.</p>
+
+          <p>Once she completed the eighth grade, she chose work over school to earn a living. Her early resume included roles as a telephone switchboard operator, a file clerk, and a package wrapper. While working, she studied dancing to become a chorus girl to make her way into show business. Although petite in stature, her shapely legs, stunning figure, and alluring eyes made her an even more attractive showgirl when she won the role in the prestigious Ziegfeld Follies at 16 years old. The elaborate theatrical revue production were famous for their display of beautiful chorus girls, known as Ziegfeld Girls, to which many female Hollywood stars would get their start.</p>
+
+          <p>After a few years as a showgirl with the Ziegfeld Follies, Ruby advanced her way to Broadway. She made her debut in 1926, and quickly landed parts small and big. Her first lead part was in a play called “The Noose” where the program introduced her as Barbara Stanwyck. Her new moniker was coined by combining the first name of her character ‘Barbara Frietchie’ with the last name of another actress in the play ‘Jane Stanwyck.’ In 1927, at the age of 20, she won a leading role in the Broadway play “Burlesque.” She received rave reviews and the play was a hit. This was the beginning of it all for Barbara. She played the part for two years and won contracts with Columbia Pictures and Warner Brothers. Author Hopkins, a well known Broadway theater director and producer remembers casting Barbara. He called her “The greatest natural actress of our time,” and “One of the theater’s great potential actresses was embalmed in celluloid.” </p>
+        </div>
+        <div className="photo-reference">
+          <img src="./images/barbara stanwyck young.jpg" alt="teenager stanwyck"/>
+        </div>
+        <div className="quotes">
+          <h3>“I knew that after fourteen I’d have to earn my own living, but I was willing to do that … I’ve always been a little sorry for pampered people, and of course, they’re ‘very’ sorry for me.”</h3>
+        </div>
+      </div>
+    )
+  }
+};
+
+export default Beginnings
