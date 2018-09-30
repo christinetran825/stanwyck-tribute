42437886674d171e95e80c58b8613cd9c34d7d3b
github username <github email address>
2018-09-30 15:31:07 -0700

    added motion pictures to homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Beginnings from './Beginnings'
+import MotionPictures from './MotionPictures'
 
 class Homepage extends Component {
   render() {
     return (
       <main>
         <Beginnings/>
+        <MotionPictures/>
       </main>
     );
   }
