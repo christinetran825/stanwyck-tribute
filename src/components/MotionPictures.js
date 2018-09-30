8f5c58074b87966b0387e157a5572a62d54b8c20
github username <github email address>
2018-09-30 15:30:30 -0700

    added motion pictures content

---
+++ src/components/MotionPictures.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class MotionPictures extends Component {
+  render() {
+    return (
+      <section id="motion-pictures">
+        <div className="photo-reference">
+          <img src="./images/baby-face-2.jpg" alt="1930s stanwyck"/>
+        </div>
+        <div className="story">
+          <h2 className="story-title">Motion Pictures</h2>
+          <p>Over her 38-year motion picture career, Barbara made 85 films playing a mix of characters usually of women from humble origins who had to fight to earn their rightful place in society. Her versatility came in comedy, drama, westerns, and thrillers. She could play a slutty, gold-digging scammer working her way up in social and financial status (Baby Face), a sleazy blonde murderous (Double Indemnity), a witty con artist pulling a fast one (The Lady Eve), and a slang-tongue screwball comedy heroine in a sort of Snow White and the Seven Dwarfs comedy way (Ball of Fire). Whatever the role in whatever genre, Barbara gives it her all, and usually on one take.</p>
+
+          <p>She developed a distinctive character image - a self-reliant, gutsy, and incredibly confident woman whose husky voice and offbeat exterior shielded a vulnerable but warm heart. Such a role can be found in the film “Stella Dallas” where she played a tough-talking but self-sacrificing noble mother. Barbara bleached her hair blonde for the leading role. This 1937 remake of the original film became so popular, a radio soap opera was created to continue the story from the film. It lasted for two decades.</p>
+
+          <p>Barbara’s crime film “Night Nurse” was one of the films sparking the Production Code formation. In the film, Barbara is featured in lingerie among other story issues. In 1934, the Production Code, also known as the Hayes Code, began to strictly enforce its beliefs and rules onto Hollywood. Their goal was to try and clean up “indecency” from movies. One would consider it an extreme case of censorship. Everything from profanity, drug use, childbirth, and interracial relationships were removed or ‘modified’ to meet code regulations. In a way, the Hayes Code fought to portray a world of happy endings embracing puritanical policies.</p>
+
+          <p>Ironically, the staunchly conservative Barbara would become a member of the Motion Picture Alliance for the Preservation of American Ideals. She believed that, “hard work with the prospect of rich reward was the American Way.” The group supported the blacklisting House Committee on Un-American Activities opposing both communist and fascist influences in Hollywood.</p>
+        </div>
+        <div className="film-gallery">
+          <img src="" alt=""/>
+        </div>
+        <div className="quotes">
+          <h3 className="testimonials">“The greatest natural actress of our time. One of the theater’s great potential actresses was embalmed in celluloid.”</h3>
+          <p className="author">Author Hopkins</p>
+          <p className="author-title">Broadway Director & Producer</p>
+        </div>
+      </section>
+    )
+  }
+};
+
+export default MotionPictures
