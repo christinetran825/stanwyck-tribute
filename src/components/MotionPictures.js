5ae67bcfb3d126e7095a2a3fa7137497a6a9c34d
github username <github email address>
2018-10-02 14:30:25 -0700

    commented out certain images

--- src/components/MotionPictures.js
+++ src/components/MotionPictures.js
@@ -20,16 +20,14 @@ class MotionPictures extends Component {
             <p>In <span className="film-name">“Ladies They Talk About”</span>, Barbara’s character was the mastermind behind a bank robbery during the Great Depression. Her performance as a calculative and impressionable Nan shows how even some women can’t get away with playing the helpless poor girl card.</p>
             <p>In the memorable <span className="film-name">“Baby Face”</span>, Barbara’s character Lilly, comes from an awful background, sexually exploited by her very own father. She fights her way out of that life by playing the helpless, innocent woman as she targets successful, older, wealthy, young, and ignorant men. She manipulates them into believing they’re in charge. Sleeping her way up to the top and getting what she wants without apologies, without showing little remorse towards morality from this character was viewed as a little too natural. Her performance seemed to have projected an idea where the everyday woman could sleep her way up to the top. </p>
             <p>Another film called <span className="film-name">“Night Nurse”</span> featured Barbara in lingerie as she undresses herself for bed. A scene in the film includes our hero being knocked unconscious after an attempted rape. The bootlegger is the hero and the chauffeur is the villain. Barbara’s nurse character is in danger of saving the children she’s caring for who are being starved and targeted for murder. On a more positive note, a maternity ward scene showcased a diverse set of women of varying ethnic backgrounds as mothers receiving their newborn babies. Night Nurse showed us a character who sought to do the moral deed of helping others but stuck in between the rules and regulations. In the end, it seems as though sliding into the amoral side proved to be the moral thing to do.</p>
+            <div className="gallery">
+              <div class="item"><img src="./images/LTTA.jpg" alt="Stanwyck in Ladies They Talk About"/></div>
+              <div class="item"><img src="./images/baby-face.png" alt="Stanwyck in Babyface"/></div>
+              <div class="item"><img src="./images/night-nurse.jpg" alt="Stanwyck in Night Nurse"/></div>
+              <div class="item"><img src="./images/double-indemnity.jpg" alt="Stanwyck in Double Indemnity"/></div>
+              <div class="item"><img src="./images/barbara-stanwyck-stage.jpeg" alt="Stanwyck in a theatre play"/></div>
+            </div>
           </article>
-
-          <div className="gallery">
-            <div class="item"><img src="./images/LTTA.jpg" alt="Stanwyck in Ladies They Talk About"/></div>
-            <div class="item"><img src="./images/baby-face.png" alt="Stanwyck in Babyface"/></div>
-            <div class="item"><img src="./images/night-nurse.jpg" alt="Stanwyck in Night Nurse"/></div>
-
-            <div class="item"><img src="./images/double-indemnity.jpg" alt="Stanwyck in Double Indemnity"/></div>
-            <div class="item"><img src="./images/barbara-stanwyck-stage.jpeg" alt="Stanwyck in a theatre play"/></div>
-          </div>
 
           <article className="precode">
             <p>As usual, these Pre-Code films and their tone were controversial to religious groups and those who preferred upholding public virtue. On screen, women were exhibiting dangerous behavior where everyday women could imitate and men would accept. Will Hays became the film industry’s spokesman whose duty was to limit government censorship. Hays developed a list of rules of behaviors but many studios didn’t always follow through. In 1933, Joseph Breen who was in charge of public relations for the Hays office began to infiltrate his beliefs of moral code and common decency into the film industry and Hollywood. He believed Hollywood was truly immoral and sought to change its ways using his connection to his religious circle. Breen and his religious group’s goal was to try and clean up “indecency” from movies. These religious leaders created their own version of a movie rating, condemning those who did not follow their heed. One could consider it an extreme case of censorship. Everything from profanity, drug use, childbirth, and interracial relationships were removed or ‘modified’ to meet code regulations. In a way, the Hays Code fought to portray a world of happy endings reflecting puritanical policies for the next 30 years.</p>
@@ -45,15 +43,15 @@ class MotionPictures extends Component {
 
             <p>She developed a distinctive character image - thick skin exterior shielding a warm, soft heart. Her emotional range was incredible. Expressing the exploration of human behavior and all its complexities was one of Barbara’s strengths not just as a performer. It might’ve been a cathartic outlet as she dug deep within her own life and experiences to bring intense complexity to her characters. Portraying a self-reliant, gutsy, and incredibly confident woman whose cool exterior held a vulnerable woman with a genuine heart defined a majority of roles she’s played. She believed writers then paid more attention to actors and actresses. Writers used to look at the actors and actresses work to build on the characters. Knowing the resume allowed writers to “know a part of you.” This was apparent in the film <span className="film-name">“Stella Dallas.”</span> Stella was a tough-talking woman but self-sacrificing noble mother. Speaking about the role, Barbara said, "...She wasn't me, that woman, but she was a woman I understood completely. She was good, cheap but good, and I could play her.” This 1937 remake of the original film was highly acclaimed and well received adding even more shine to Barbara’s star. The film was so popular a radio soap opera was created to continue the story from the film. It lasted for two decades. Stella Dallas is one of Barbara's most well-known role earning her an Academy Award Nomination.</p>
 
-            {/* <div className="gallery">
-              <img src="./images/wrong-number.jpeg" alt="Stanwyck in Double Indemnity" className="photo-reference"/>
-              <img src="./images/stella-dallas-2.jpg" alt="Stanwyck in a theatre play" className="photo-reference"/>
-              <img src="./images/annie-oakley.jpg" alt="Stanwyck in a theatre play" className="photo-reference"/>
-              <img src="./images/the-lady-eve.jpg" alt="Stanwyck in a theatre play" className="photo-reference"/>
-              <img src="./images/thorn-birds.jpg" alt="Stanwyck in a theatre play" className="photo-reference"/>
-              <img src="./images/ball-of-fire.jpg" alt="Stanwyck in a theatre play" className="photo-reference"/>
-              <img src="./images/big-valley.jpg" alt="Stanwyck in a theatre play" className="photo-reference"/>
-            </div> */}
+            <div className="gallery">
+              <img src="./images/wrong-number.jpg" alt="Stanwyck in Wrong Number" id="a"/>
+              <img src="./images/stella-dallas-2.jpg" alt="Stanwyck in Stella Dallas" id="b"/>
+              {/* <img src="./images/annie-oakley.jpg" alt="Stanwyck in Annie Oakley" id="c"/> */}
+              <img src="./images/thorn-birds.jpg" alt="Stanwyck in Thorn Birds" id="d"/>
+              <img src="./images/ball-of-fire.jpg" alt="Stanwyck in Ball of Fire" id="e"/>
+              <img src="./images/the-lady-eve.jpg" alt="Stanwyck in Lady Eve" id="f"/>
+              {/* <img src="./images/big-valley.jpg" alt="Stanwyck in Big Valley"/> */}
+            </div>
 
             <article id="testimonials" className="quotes">
               <h3 className="quote">“The greatest natural actress of our time. One of the theater’s great potential actresses was embalmed in celluloid.”</h3>
