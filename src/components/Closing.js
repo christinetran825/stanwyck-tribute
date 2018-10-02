d592270086bfd5d5797664be1dede28f31d65ef7
github username <github email address>
2018-10-02 14:30:09 -0700

    changed section to div

--- src/components/Closing.js
+++ src/components/Closing.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 class Closing extends Component {
   render() {
     return (
-      <section id="closing">
+      <div id="closing">
         <div className="section-hero">
           {/* <img src="./images/barbara-stanwyck-portrait-3.jpg" alt="1930s stanwyck"/> */}
           <img src="./images/barbara-stanwyck.gif" alt="gif of stanwyck blowing kisses"/>
         </div>
-      </section>
+      </div>
     )
   }
 };
