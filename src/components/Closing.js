625e647a6a5d598699fc141acb34f42a3935b949
github username <github email address>
2018-10-03 11:23:07 -0700

    added references; applied Free Code Camp requirements

--- src/components/Closing.js
+++ src/components/Closing.js
@@ -8,6 +8,16 @@ class Closing extends Component {
           {/* <img src="./images/barbara-stanwyck-portrait-3.jpg" alt="1930s stanwyck"/> */}
           <img src="./images/barbara-stanwyck.gif" alt="gif of stanwyck blowing kisses"/>
         </div>
+        <div className="story" id="tribute-info">
+          <p className="article-title">References</p>
+          <ul>
+            <li><a href="https://www.thehairpin.com/2013/03/scandals-of-classic-hollywood-the-many-faces-of-barbara-stanwyck/" id="tribute-link" target="_blank">The Hairpin</a></li>
+            <li><a href="https://filmdialogueone.wordpress.com/2017/10/24/barbara-stanwyck/" id="tribute-link" target="_blank">Film Dialogue</a></li>
+            <li><a href="https://www.bestmoviesbyfarr.com/articles/barbara-stanwyck-bio/2016/04" id="tribute-link" target="_blank">Best Movies By Farr</a></li>
+            <li><a href="https://www.barbara-stanwyck.com/essential-guide-barbara-stanwyck/" id="tribute-link" target="_blank">The Essential Guide to Barbara Stanwyck</a></li>
+            <li><a href="https://www.nytimes.com/2013/11/14/books/a-life-of-barbara-stanwyck-by-victoria-wilson.html" id="tribute-link" target="_blank">Los Angeles Times Book Review - "A Life of Barbara Stanwyck"</a></li>
+          </ul>
+        </div>
       </div>
     )
   }
